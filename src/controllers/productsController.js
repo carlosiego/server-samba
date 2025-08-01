9d78162a67e44cdde52a4836246bd65576445360
Carlos <carlosiego2017@gmail.com>
2025-08-01 09:48:36 -0300

    FIX

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-const client = require('../redis')
 
 class imagesProductsController {
 
     static getImageProducts = async (req, res) => {
 
-        const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-        const imageFromCache = await client.get(fullUrl)
-        if (imageFromCache) {
-            return res.json(JSON.parse(imageFromCache))
-        }
         const { code } = req.params 
         const nameImage = code + '.png'
         let directory;
@@ -23,23 +17,11 @@ class imagesProductsController {
         
         fs.access(pathFile, fs.constants.F_OK, async (err) => {
             if (err) {
-                await client.set(fullUrl, JSON.stringify(
-                    {
-                        error: true,
-                        urlImage: `http://${process.env.SERVER_ADDRESS}:${process.env.PORT}/files/not-found.png`,
-                    }
-                ), { EX: process.env.EXPIRATION})
                 res.json({
                     error: true,
                     urlImage: `http://${process.env.SERVER_ADDRESS}:${process.env.PORT}/files/not-found.png`,
                 })
             } else {
-                await client.set(fullUrl, JSON.stringify(
-                    {
-                        error: false,
-                        urlImage: `http://${process.env.SERVER_ADDRESS}:${process.env.PORT}/files/${nameImage}`,
-                    }
-                ), { EX: process.env.EXPIRATION})
                 res.json({
                     error: false,
                     urlImage: `http://${process.env.SERVER_ADDRESS}:${process.env.PORT}/files/${nameImage}`,
